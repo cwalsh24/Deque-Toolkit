9ec9460186effbcd1898468f8db693cfe4bdc698
Connor Walsh <cwalsh24@ut.utm.edu>
2022-12-02 09:31:50 -0600

    more updates

--- driver.cpp
+++ driver.cpp
@@ -15,6 +15,7 @@ using namespace std;
 int main()
 {
   deque d;
+  //tests for the empty() and size() methods
   cout << "Test for the empty() and size() methods: " << endl << endl;
   
   //if the empty() method evaluates to false, we know that the deque is starting empty
@@ -39,8 +40,8 @@ int main()
   
   cout << endl << endl;
   
-  cout << "Front and back method tests: " << endl << endl;
-  
+  //tests for the front and back methods
+  cout << "Front and back method tests: " << endl << endl;  
   cout << "The number at the front of the deque is " << d.front() << "." << endl;
   cout << "The number at the back of the deque is " << d.back() << "." << endl << endl;
 
@@ -50,7 +51,8 @@ int main()
   cout << "If deque[0] is set to equal 400..." << endl;
   d[0] = 400;
   cout << "The number at index zero is " << d[0] << "." << endl << endl;
-  
+
+  //pops 999 numbers 
   cout << "Test for pop_back: 999 elements popped:" << endl << endl;
     for(int i = 0; i < 999; i++){
     d.pop_back();
@@ -62,7 +64,8 @@ int main()
   }
   
   cout << endl << endl;
-
+  
+  //shows both sides of the deque with a few numbers in them
   cout << "Reading in a few more integers..." << endl;
   for(int i = 0; i < 5; i++){
     d.push_back(i);
@@ -95,7 +98,6 @@ int main()
   cout << "There are now " << d.getSize()<< " elements in the deque." << endl << endl;
 
   cout << "Test for pop_front method: " << endl << endl;
-
   cout << "If 499 numbers are popped..." << endl << endl;
   for(int i = 0; i < 499; i++){
     d.pop_front();
@@ -110,80 +112,5 @@ int main()
 
   //cout << endl << endl;
   
-  /** for(int i = 0; i < 1000; i++){
-    d.pop_front();
-    } 
-  
-  //prints the integers contained inside of the deque
-  cout << "Done. Printing deque..." << endl << endl;
-  for(int i = 0; i < d.getSize(); i++){
-  cout << d[i] << " ";
-  }
-  
-  cout << endl << endl; 
-  
-  cout << "The size of the deque is currently " << d.getSize() << "." << endl << endl;
-  
-  **/
-
-  /**
-  //tests front and back
-  cout << "Front and back method tests: " << endl << endl;
-  
-  cout << "The number at the front of the deque is " << d.front() << "." << endl;
-  cout << "The number at the back of the deque is " << d.back() << "." << endl << endl;
-
-  //tests the brace operator
-  cout << "Brace operator test: Changing a value in the deque" << endl << endl;
-  cout << "The number at index zero is " << d[0] << "." << endl;
-  cout << "If deque[0] is set to equal 400..." << endl;
-  d[0] = 400;
-  cout << "The number at index zero is " << d[0] << "." << endl << endl;
-
-  //pops the first 997 elements
-  cout << "Test for the pop_front() method:" << endl;
-  cout << "If 997 elements are popped from the front of the deque and the first 5 elements are printed..."
-  << endl << endl;
-  for(int i = 0; i < d.getSize() - 3; i++){
-  d.pop_front();
-  }
-  //prints the first 5 elements contained in the deque
-  for(int i = 0; i < 5; i++){
-  cout << d[i] << " ";
-  }
-  cout << endl << endl;
-  
-  cout << "The size of the deque is currently " << d.getSize()
-  << " and we can see that that 2 is now at the front of the deque." << endl << endl;
-  
-  cout << "Behind 2 1 0 you can see zeros which are just empty indexes" << endl;
-  
-  //pushes 10 more integers into the deque
-  for(int i = 0; i < 10; i++){
-  d.push_front(2 * 2);
-  }
-
-  //
-  for(int i = 0; i < 13; i++){
-  cout << d[i] << " ";
-  }
-  
-  cout << endl;
-  cout << "From this we can see that the new integers have filled the deque" << endl;
-  
-  //pushes 1000 integers into the deque
-  cout << "push_back test with 1000 integers: " << endl << endl;
-  cout << "Reading in integers..." << endl;
-  for(int i = 0; i < 1000; i++){
-  d.push_back(i);
-  }
-  
-  //prints the integers contained inside of the deque
-  cout << "Done. Printing deque again..." << endl << endl;
-  for(int i = 0; i < d.getSize(); i++){
-  cout << d[i] << " ";
-  }
-  cout << endl;
-  **/
   return 0;
 }
