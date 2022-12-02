2ae19cbd8c017a72102272fe02415dcf9498da05
Kyle Byassee <kylhbyas@ut.utm.edu>
2022-12-01 19:08:36 -0600

    compiles again

--- deque.cpp
+++ deque.cpp
@@ -11,30 +11,34 @@
 #include <iostream>
 
 deque::deque(){
+  // Empty state
   size = 0;
   
-  //sets the number of rows, 8 seemed like a good number.
+  // Number of rows, 8 seemed like a good number
   mapSize = 8;
-
-  //sets the starting value for the first_block, which is the middle one in the blockmap
+  
+  // Middle row in the blockmap
   first_block = mapSize / 2;
-
+  
+  // Empty condition
   last_block = first_block;
-
-  //sets the amount of entries allowed per block, 128 seemed like a good number.
+  
+  // sets the amount of entries allowed per block, 128 seemed like a good number.
   blockSize = 128;
   
-  //the middle of the row seemed to be a good starting point
+  // Beginning of middle row
   first_element = 0;
-
-  //last_element == first_element to start
+  
+  // Empty condition
   last_element = first_element;
   
-  //initializes the 2D array with mapSize number of rows
+  // Initializes the 2D array with mapSize number of rows
   blockmap = new int *[mapSize];
-
-  //starting position is the middle of the middle block.
-  blockmap[first_block] = new int[blockSize];
+  
+  // Initializes each row
+  for(int i = 0; i < mapSize; i++){
+    blockmap[i] = new int[blockSize];
+  }
 }
 
 deque::~deque(){
@@ -58,13 +62,13 @@ bool deque::empty(){
 }
 
 void deque::push_front(int number){
-  // If size == 0
+  // If empty
   if(size == 0){
     blockmap[first_block][first_element] = number;
     size++;
     return;
   }
-
+  
   // If end of row is not reached
   if(first_element > 0){
     first_element--;
@@ -77,58 +81,36 @@ void deque::push_front(int number){
   if(first_block > 0){
     first_block--;
     first_element = blockSize - 1;
-    
-    //initialize the previous row.
-    //blockmap[first_block] = new int[blockSize];
-    
     blockmap[first_block][first_element] = number;
     size++;
     return;
   }
   
-  // If first block == 0 ... make a new map
+  // If beginning of first row is reached, then resize
   mapSize *= 2;
   int **mapReplacement = new int*[mapSize];
-  first_block = mapSize / 2;
-  mapReplacement[first_block] = new int[blockSize];
+  
+  // Initialize the new map
+  for(int i = 0; i < mapSize; i++){
+    mapReplacement[i] = new int[blockSize];
+  }
+  
   // Copy all of the values in blockmap
   for(int i = 0; i < mapSize / 2; i++){
     mapReplacement[i] = blockmap[i];
   }
-  //blockmap points at the new map
-  blockmap = mapReplacement;
-
-  // Sets the indexes
-  first_block--;
-  first_element = blockSize - 1;
   
-  //number is stored in the blockmap
+  // blockmap points at the new map
+  blockmap = mapReplacement;
+  
+  // Store number in the blockmap
+  first_element = blockSize - 1;
   blockmap[first_block][first_element] = number;
   size++;
 }
 
 void deque::push_back(int number){ //not working right
-  //if the first and last element values are the same then the element is inserted
-  //in the index to the right of the first element. 
-  /**  if(first_element == last_element){
-    last_element++;
-    blockmap[first_block][last_element] = number;
-    //size is increased to account for the new element. 
-    size++;
-  }
-  else{
-    //if the last element value is less than the blockSize (meaning that the row block isn't full)
-    //then the new element can be inserted in this position. We need the minus 1 due to the array.
-    if(last_element < blockSize - 1){
-      blockmap[first_block][last_element] = number;
-      last_element++;
-      size++;
-    }
-    //if the block is full... 
-    
-    }**/
-  
-  
+  // If empty
   if(size == 0){
     blockmap[last_block][last_element] = number;
     size++;
@@ -144,7 +126,7 @@ void deque::push_back(int number){ //not working right
   }
   
   // If end of row is reached && no resize
-  if(last_block < mapSize){
+  if(last_block < mapSize - 1){
     last_block++;
     last_element = 0;
     blockmap[last_block][last_element] = number;
@@ -152,11 +134,14 @@ void deque::push_back(int number){ //not working right
     return;
   }
   
-  // If last block == mapSize then resize
+  // If end of last row is reached, then resize
   mapSize *= 2;
   int **mapReplacement = new int*[mapSize];
-  last_block = mapSize / 2;
-  mapReplacement[last_block] = new int[blockSize];
+  
+  // Initialize the new map
+  for(int i = 0; i < mapSize; i++){
+    mapReplacement[i] = new int[blockSize];
+  }
   
   // Copy all of the values in blockmap
   for(int i = 0; i < mapSize / 2; i++){
@@ -166,11 +151,11 @@ void deque::push_back(int number){ //not working right
   // blockmap points at the new map
   blockmap = mapReplacement;
 
-  // Sets the indexes
+  // Set the indexes
   last_block++;
   last_element = 0;
 
-  // Stores number
+  // Store number in the blockmap
   blockmap[last_block][last_element] = number;
   size++;
 }
@@ -179,7 +164,8 @@ void deque::pop_front(){
   if(size == 0){
     return;
   }
-  //shifts the first_element value right
+  // Shifts first_element right
+  // The first_element is free to be overwritten
   first_element++;
   size--;
 }
@@ -188,7 +174,8 @@ void deque::pop_back(){
   if(size == 0){
     return;
   }
-  //shifts the first_element value left
+  // Shifts last_element left
+  // The last_element is free to be overwritten
   last_element--;
   size--;
 }
@@ -201,13 +188,11 @@ int& deque::operator[](int i){
   return blockmap[row][column];
 }
 
-// Calls the [] operator
 int deque::front(){
   return operator[](0);
 }
 
-// Calls the [] operator
-// size - 1 for 0 based indexing
 int deque::back(){
+  // size - 1 for 0 based indexing
   return operator[](size - 1);
 }
--- driver.cpp
+++ driver.cpp
@@ -16,74 +16,89 @@ int main()
 {
   deque d;
   cout << "Test for the empty() and size() methods: " << endl << endl;
+  
   //if the empty() method evaluates to false, we know that the deque is starting empty
   if (d.empty() == true){
     cout << "The deque is currently empty. " << endl;
   }
+  
   //prints the size using the size() method to show the user that the deque is empty
   cout << "The size of the deque is currently " << d.getSize() << "." << endl << endl;
-
+  
+  //pushes 1000 integers into the deque
   cout << "push_front test with 1000 integers: " << endl << endl;
   cout << "Reading in integers..." << endl;
-  cout << "Done. Printing deque..." << endl << endl;
-
-  //pushes 1000 integers into the deque 
   for(int i = 0; i < 1000; i++){
     d.push_front(i);
   }
-
-  //prints the integers contained inside of the deque 
-  for(int i = 0; i < 1000; i++){
+  
+  //prints the integers contained inside of the deque
+  cout << "Done. Printing deque..." << endl << endl;
+  for(int i = 0; i < d.getSize(); i++){
     cout << d[i] << " ";
   }
   
   cout << endl << endl;
-
+  
   cout << "The size of the deque is currently " << d.getSize() << "." << endl << endl;
-
+  
+  //tests front and back
   cout << "Front and back method tests: " << endl << endl;
+  
   cout << "The number at the front of the deque is " << d.front() << "." << endl;
   cout << "The number at the back of the deque is " << d.back() << "." << endl << endl;
 
-  cout << "Operator test: Changing a value in the deque" << endl << endl; 
+  //tests the brace operator
+  cout << "Brace operator test: Changing a value in the deque" << endl << endl;
   cout << "The number at index zero is " << d[0] << "." << endl;
-  cout << "If deque[0] is set to equal 400..." << endl; 
+  cout << "If deque[0] is set to equal 400..." << endl;
   d[0] = 400;
   cout << "The number at index zero is " << d[0] << "." << endl << endl;
 
-  cout << "Test for the pop_front() method:" << endl; 
+  //pops the first 997 elements
+  cout << "Test for the pop_front() method:" << endl;
   cout << "If 997 elements are popped from the front of the deque and the first 5 elements are printed..."
        << endl << endl;
-
-  int dequeSize = d.getSize();
-  
-  //pops the first 997 elements
-  for(int i = 0; i < dequeSize - 3; i++){
+  for(int i = 0; i < d.getSize() - 3; i++){
     d.pop_front();
   }
-
-  //dequeSize = d.getSize();
-  
-  //prints the elements contained in the deque
+  //prints the first 5 elements contained in the deque
   for(int i = 0; i < 5; i++){
     cout << d[i] << " ";
   }
   cout << endl << endl;
-
-  cout << "The size of the deque is currently " << d.getSize() <<
-    " and we can see that that 2 is now at the front of the deque." << endl << endl;
+  
+  cout << "The size of the deque is currently " << d.getSize()
+       << " and we can see that that 2 is now at the front of the deque." << endl << endl;
+  
   cout << "Behind 2 1 0 you can see zeros which are just empty indexes" << endl;
-  /**
+  
   //pushes 10 more integers into the deque
   for(int i = 0; i < 10; i++){
     d.push_front(2 * 2);
   }
 
+  //
   for(int i = 0; i < 13; i++){
     cout << d[i] << " ";
   }
+  
   cout << endl;
-  cout << "From this we can see that the new integers have filled the deque" << endl; 
-  **/ 
+  cout << "From this we can see that the new integers have filled the deque" << endl;
+  
+  //pushes 1000 integers into the deque
+  cout << "push_back test with 1000 integers: " << endl << endl;
+  cout << "Reading in integers..." << endl;
+  for(int i = 0; i < 1000; i++){
+    d.push_back(i);
+  }
+  
+  //prints the integers contained inside of the deque
+  cout << "Done. Printing deque again..." << endl << endl;
+  for(int i = 0; i < d.getSize(); i++){
+    cout << d[i] << " ";
+  }
+  cout << endl;
+  
   return 0;
 }
